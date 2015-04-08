10c8e04daa94f2e4c2ef55be2871e17c4fdbcb8e
mrkirby153 <mr.austinwhyte@gmail.com>
2015-04-07 17:47:42 -0700

    Moved Chieves to ChievesMod to avoid confusion

--- src/main/java/me/mrkirby153/chieves/ChievesMod.java
+++ src/main/java/me/mrkirby153/chieves/ChievesMod.java
@@ -1,16 +1,18 @@
 package me.mrkirby153.chieves;
 
+import me.mrkirby153.chieves.achievement.Chieves;
 import me.mrkirby153.chieves.proxy.CommonProxy;
 import me.mrkirby153.chieves.reference.Reference;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name=Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
-public class Chieves {
+public class ChievesMod {
 
     @Mod.Instance(Reference.MOD_ID)
-    public static Chieves instance;
+    public static ChievesMod instance;
 
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
@@ -19,5 +21,11 @@ public class Chieves {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
         proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+        // TODO: Add initializations to proxy
+        Chieves.initalize();
     }
 }
