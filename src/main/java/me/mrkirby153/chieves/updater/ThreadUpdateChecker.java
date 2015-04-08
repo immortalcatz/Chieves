53f122bd9e5003254aaf60e267277924572e4570
mrkirby153 <mr.austinwhyte@gmail.com>
2015-04-07 17:47:55 -0700

    Finished update checker

--- src/main/java/me/mrkirby153/chieves/updater/ThreadUpdateChecker.java
+++ src/main/java/me/mrkirby153/chieves/updater/ThreadUpdateChecker.java
@@ -1,5 +1,14 @@
 package me.mrkirby153.chieves.updater;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import me.mrkirby153.chieves.reference.Reference;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class ThreadUpdateChecker extends Thread {
 
     public ThreadUpdateChecker() {
@@ -12,6 +21,16 @@ public class ThreadUpdateChecker extends Thread {
     public void run() {
         // Get the last successful build
         try {
+            // Download the JSON into a json list
+            URL url = new URL(UpdateChecker.updateCheckFile);
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(new JsonReader(new InputStreamReader(url.openStream())));
+            JsonObject overallObject = element.getAsJsonObject();
+            JsonElement obj = overallObject.get("1.8");
+            String version = obj.getAsString();
+            if(!version.equalsIgnoreCase(Reference.VERSION)){
+                UpdateChecker.newVersion = version;
+            }
         } catch (Exception e) {
             UpdateChecker.updateFailure = true;
             e.printStackTrace();
--- src/main/java/me/mrkirby153/chieves/updater/UpdateChecker.java
+++ src/main/java/me/mrkirby153/chieves/updater/UpdateChecker.java
@@ -1,8 +1,11 @@
 package me.mrkirby153.chieves.updater;
 
+import me.mrkirby153.chieves.reference.Reference;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -10,13 +13,11 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 public class UpdateChecker {
 
     public static final String updateCheckFile = "https://dl.dropboxusercontent.com/u/121954827/modding/Chieves/version.json";
-    public static final String updateDownloadUrl = ""; // Download from jenkins?
 
     public static boolean updateDone = false;
     public static boolean updateFailure = false;
-    public static boolean updateAvalable = false;
     public static boolean notifiedPlayer = false;
-    public static int newVersion = -1;
+    public static String newVersion = null;
 
     public void init() {
         new ThreadUpdateChecker();
@@ -25,7 +26,7 @@ public class UpdateChecker {
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
-        if (updateDone && event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null && !notifiedPlayer && !updateFailure) {
+        /*if (updateDone && event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null && !notifiedPlayer && !updateFailure) {
             EntityPlayer player = Minecraft.getMinecraft().thePlayer;
             player.addChatComponentMessage(new ChatComponentText("UPDATE! " + newVersion));
             notifiedPlayer = true;
@@ -33,6 +34,17 @@ public class UpdateChecker {
         if(updateDone && event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null && !notifiedPlayer && updateFailure){
             EntityPlayer player = Minecraft.getMinecraft().thePlayer;
             player.addChatComponentMessage(new ChatComponentText("UPDATE FAILED!"));
+            notifiedPlayer = true;
+        }*/
+        if (updateDone && event.phase == TickEvent.Phase.END && Minecraft.getMinecraft().thePlayer != null && !notifiedPlayer) {
+            EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+            if (updateFailure) {
+                player.addChatComponentMessage(new ChatComponentTranslation("version.updateFailed").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED).setBold(true)));
+            } else {
+                if (newVersion != null) {
+                    player.addChatComponentMessage(new ChatComponentTranslation("version.updateFound", newVersion, Reference.VERSION).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.AQUA)));
+                }
+            }
             notifiedPlayer = true;
         }
     }
